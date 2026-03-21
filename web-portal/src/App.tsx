8c86058f1c64d424bcc41c6b65f3b8914380b2da
tinkhmt <trantrongtin201005@gmail.com>
2026-03-21 20:59:48 +0700

    remove UI for hr users

--- web-portal/src/App.tsx
+++ web-portal/src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster } from 'react-hot-toast'
 import { AuthProvider } from '@/contexts/AuthContext'
 import { AuthLayout } from '@/layouts/AuthLayout'
 import { ProtectedRoute } from '@/components/ProtectedRoute'
-import { HrRoute } from '@/components/HrRoute'
 import { LoginPage } from '@/pages/LoginPage'
 import { RegisterPage } from '@/pages/RegisterPage'
 import { HomePage } from '@/pages/HomePage'
@@ -12,12 +11,6 @@ import { JobDetailPage } from '@/pages/JobDetailPage'
 import { CompaniesPage } from '@/pages/CompaniesPage'
 import { CompanyDetailPage } from '@/pages/CompanyDetailPage'
 import { LandingPage } from '@/pages/LandingPage'
-import { HrDashboardPage } from '@/pages/hr/HrDashboardPage'
-import { HrJobsPage } from '@/pages/hr/HrJobsPage'
-import { HrJobDetailPage } from '@/pages/hr/HrJobDetailPage'
-import { HrJobFormPage } from '@/pages/hr/HrJobFormPage'
-import { HrApplicationsPage } from '@/pages/hr/HrApplicationsPage'
-import { HrApplicationDetailPage } from '@/pages/hr/HrApplicationDetailPage'
 
 function AppRoutes() {
   return (
@@ -57,43 +50,6 @@ function AppRoutes() {
         <ProtectedRoute>
           <HomePage />
         </ProtectedRoute>
-      } />
-
-      {/* HR Routes */}
-      <Route path="/hr" element={
-        <HrRoute>
-          <HrDashboardPage />
-        </HrRoute>
-      } />
-      <Route path="/hr/jobs" element={
-        <HrRoute>
-          <HrJobsPage />
-        </HrRoute>
-      } />
-      <Route path="/hr/jobs/new" element={
-        <HrRoute>
-          <HrJobFormPage />
-        </HrRoute>
-      } />
-      <Route path="/hr/jobs/:id" element={
-        <HrRoute>
-          <HrJobDetailPage />
-        </HrRoute>
-      } />
-      <Route path="/hr/jobs/:id/edit" element={
-        <HrRoute>
-          <HrJobFormPage />
-        </HrRoute>
-      } />
-      <Route path="/hr/applications" element={
-        <HrRoute>
-          <HrApplicationsPage />
-        </HrRoute>
-      } />
-      <Route path="/hr/applications/:id" element={
-        <HrRoute>
-          <HrApplicationDetailPage />
-        </HrRoute>
       } />
 
       <Route path="*" element={<Navigate to="/" replace />} />
